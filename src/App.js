83e03c3c7c794b3591337ec36e15a31211b50c34
jongmin <ace4463@gmail.com>
2020-06-09 13:10:22 +0900

    key props 추가

--- src/App.js
+++ src/App.js
@@ -4,33 +4,41 @@ function Food({name, image}){
   return(
     <div>
     <h1>I like {name}</h1>;
-    <img src={image}/>
+    <img src={image} alt={name}/>
     </div>
   ); 
 }
 
 const foodILike = [
   {
+    id: 1,
     name: 'Kimchi',
     image: 'https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png',
   },
   {
+    id: 2,
     name: 'Bulgogi',
     image: 'https://recipe1.ezmember.co.kr/cache/recipe/2015/04/18/c2aac4feb7c5fe07b8d118311cc8141a1.jpg',
   },
   {
+    id: 3,
     name: 'Bibimbap',
     image: 'https://recipe1.ezmember.co.kr/cache/recipe/2018/10/03/355b5cd5c3beb1a775c82ee425dcd1931.jpg',
   }
   
 ];
 
+// function renderFood(dish){
+//   return <Food name= {dish.name} image={dish.image}/>;
+// }
+
+// const renderFood = dish => <Food name={dish.name} image={dish.image}/>
+
 function App() {
   return(
     <div>
-      {foodILike.map(dish => (<
-        Food name={dish.name} image={dish.image}/>
-        ))}
+      {foodILike.map(dish => (<Food key={dish.id} name={dish.name} image={dish.image}/>
+      ))}
     </div>
   ); 
 }
