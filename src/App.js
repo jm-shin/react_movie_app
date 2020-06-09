13659d1a0937cf59c71a898dc14bf4c4681734f7
jongmin <ace4463@gmail.com>
2020-06-09 10:20:12 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
-function Food(props){
-  var {fav} = props;
-  return <h1>I like {fav}</h1>;
+function Food({name, image}){
+  return(
+    <div>
+    <h1>I like {name}</h1>;
+    <img src={image}/>
+    </div>
+  ); 
 }
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png',
+  },
+  {
+    name: 'Bulgogi',
+    image: 'https://recipe1.ezmember.co.kr/cache/recipe/2015/04/18/c2aac4feb7c5fe07b8d118311cc8141a1.jpg',
+  },
+  {
+    name: 'Bibimbap',
+    image: 'https://recipe1.ezmember.co.kr/cache/recipe/2018/10/03/355b5cd5c3beb1a775c82ee425dcd1931.jpg',
+  }
+  
+];
 
 function App() {
   return(
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi"/>
+      {foodILike.map(dish => (<
+        Food name={dish.name} image={dish.image}/>
+        ))}
     </div>
   ); 
 }
