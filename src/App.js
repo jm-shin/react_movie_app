83a937ad5e8149e570998632b3f4f043e116c903
jongmin <ace4463@gmail.com>
2020-06-10 10:05:28 +0900

    prop-types 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({name, image}){
+function Food({name, picture,rating}){
   return(
     <div>
     <h1>I like {name}</h1>;
-    <img src={image} alt={name}/>
+    <img src={picture} alt={name}/>
     </div>
   ); 
 }
@@ -14,16 +15,19 @@ const foodILike = [
     id: 1,
     name: 'Kimchi',
     image: 'https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png',
+    rating: 5,
   },
   {
     id: 2,
     name: 'Bulgogi',
     image: 'https://recipe1.ezmember.co.kr/cache/recipe/2015/04/18/c2aac4feb7c5fe07b8d118311cc8141a1.jpg',
+    rating: 4.9,
   },
   {
     id: 3,
     name: 'Bibimbap',
     image: 'https://recipe1.ezmember.co.kr/cache/recipe/2018/10/03/355b5cd5c3beb1a775c82ee425dcd1931.jpg',
+    rating: 3.8,
   }
   
 ];
@@ -37,10 +41,17 @@ const foodILike = [
 function App() {
   return(
     <div>
-      {foodILike.map(dish => (<Food key={dish.id} name={dish.name} image={dish.image}/>
+      {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} 
+      rating={dish.rating}/>
       ))}
     </div>
   ); 
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 export default App;
