9ca34e97f2235aa31c92a60f204702a8a39d874e
jongmin <ace4463@gmail.com>
2020-07-01 10:09:05 +0900

    create router

---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.css';
+
+function About(){
+    return (
+        <div className="about__container">
+            <span>
+                "If you cannot fly then run. If you cannot run, then walk. And, 
+                if you cannot walk, then crawl, but whatever you do, you have to keep moving forward."
+            </span>
+            <span>- Martin Luther King Jr.</span>
+        </div>
+    )
+}
+
+export default About;
