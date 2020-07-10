e3174661523cc5c42ba4c9342a6449930164aec9
jongmin <ace4463@gmail.com>
2020-07-10 10:24:32 +0900

    Updates

---
+++ precache-manifest.e2c770f400fae189592265c9c9c3b840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a24bef68c72bf37af4c19ffb613b89",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "3dbefe13c4595e2e3b65",
+    "url": "/react_movie_app/static/css/main.219db9fa.chunk.css"
+  },
+  {
+    "revision": "2693e80f42fb0ab95803",
+    "url": "/react_movie_app/static/js/2.dd2c73a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_movie_app/static/js/2.dd2c73a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dbefe13c4595e2e3b65",
+    "url": "/react_movie_app/static/js/main.9b2f1668.chunk.js"
+  },
+  {
+    "revision": "6326c3411a515ea57f93",
+    "url": "/react_movie_app/static/js/runtime-main.04b48da3.js"
+  }
+]);
